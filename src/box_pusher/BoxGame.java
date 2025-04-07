eb37f3e8d9f9d924691f050b4e27998b649e21a7
Allen Long <jallenlong05@gmail.com>
2025-04-07 13:33:10 -0500

    Slightly modified the tile interface.

---
+++ src/box_pusher/BoxGame.java
@@ -0,0 +1,13 @@
+package box_pusher;
+import edu.macalester.graphics.CanvasWindow;
+/**
+ * The main class that handles things like managing the tile array, and player movement
+ */
+public class BoxGame {
+    private Tile[][] tileArray;
+    private CanvasWindow canvas;
+
+    public BoxGame(){
+        
+    }
+}
--- src/box_pusher/Tile.java
+++ src/box_pusher/Tile.java
@@ -1,4 +1,5 @@
 package box_pusher;
+import java.awt.Color;
 /**
  * An interface for representing the different tiles on the game board.
  */
@@ -9,10 +10,21 @@ public interface Tile {
      */
     public String getSignifier();
 
+    
+
     /** 
-     * behavior for what to do when something tries to interact, or move through, this tile.
+     * Behavior for what to do when something tries to interact, or move through, this tile.
+     * @param otherTile The tile that is trying to interact with this one.
+     * @param tileArray The array of tiles to modify.
      */
     public void interact(Tile otherTile, Tile[][] tileArray);
+
+
+    /**
+     * Returns the color of the tile so it can be drawn on the screen.
+     */
+    public Color getColor();
+    
     
 
 }
