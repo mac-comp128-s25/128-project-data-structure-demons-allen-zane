fdf07e28dabf70749dcab7d81f83a3dde6fd14eb
Allen Long <jallenlong05@gmail.com>
2025-04-16 11:27:24 -0500

    Player input is now working

--- src/boxpusher/BoxGame.java
+++ src/boxpusher/BoxGame.java
@@ -1,5 +1,6 @@
 package boxpusher;
 import edu.macalester.graphics.CanvasWindow;
+import edu.macalester.graphics.events.Key;
 /**
  * The main class that handles things like managing the tile array, and player movement
  */
@@ -23,9 +24,31 @@ public class BoxGame {
     public static void main(String[] args) {
         BoxGame boxGame = new BoxGame();
         TileGraphics.showTiles(boxGame.getTileArray(), boxGame.getCanvas());
-
-        Tile playerTile = Levels.getPlayerTile(boxGame.getTileArray());
         
+      
+        
+        boxGame.getCanvas().onKeyDown(event -> boxGame.move(event.getKey()));
+    }
+
+    private void move(Key key) {
+        Tile playerTile = Levels.getPlayerTile(tileArray);
+        if (key.equals(Key.valueOf("DOWN_ARROW")) &&  playerTile.getIndex1()+1 <  tileArray.length){
+            Tile interactTile = tileArray[playerTile.getIndex1()+1][playerTile.getIndex2()];
+            playerTile.interact(interactTile, tileArray);
+        }
+        if (key.equals(Key.valueOf("LEFT_ARROW")) &&  playerTile.getIndex2()-1 >= 0){
+            Tile interactTile = tileArray[playerTile.getIndex1()][playerTile.getIndex2()-1];
+            playerTile.interact(interactTile, tileArray);
+        }
+        if (key.equals(Key.valueOf("UP_ARROW")) &&  playerTile.getIndex1()-1 >=0){
+            Tile interactTile = tileArray[playerTile.getIndex1()-1][playerTile.getIndex2()];
+            playerTile.interact(interactTile, tileArray);
+        }
+        if (key.equals(Key.valueOf("RIGHT_ARROW")) &&  playerTile.getIndex2()+1 < tileArray[0].length){
+            Tile interactTile = tileArray[playerTile.getIndex1()][playerTile.getIndex2()+1];
+            playerTile.interact(interactTile, tileArray);
+        }
+        TileGraphics.showTiles(tileArray, canvas);
     }
 
 }
